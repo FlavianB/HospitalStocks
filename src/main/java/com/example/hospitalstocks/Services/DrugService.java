8202293eff4c9b89c5c7db032806e6bf42c23f49
FlavianB <flaviburca@gmail.com>
2024-06-04 01:22:54 +0300

    updated drug service

--- src/main/java/com/example/hospitalstocks/Services/DrugService.java
+++ src/main/java/com/example/hospitalstocks/Services/DrugService.java
@@ -19,7 +19,7 @@ public class DrugService {
     public Page<Drug> getAllDrugs(String name, Pageable pageable) {
         Page<Drug> drugsPage = name.isEmpty() ?
                 drugRepository.findAll(pageable) :
-                drugRepository.findByNameContainingIgnoreCaseOrMainComponentContainingIgnoreCase(name, name, pageable);
+                drugRepository.findByNameContainingIgnoreCase(name, pageable);
         return drugsPage;
     }
 
